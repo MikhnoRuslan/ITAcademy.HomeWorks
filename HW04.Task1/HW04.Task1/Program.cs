e774fd4a52e733f2d8d9459e46d41250d77261ef
MikhnoRuslan <r.mikhno92@gmail.com>
2020-06-08 15:06:16 +0300

    Completer all tasks

---
+++ HW04.Task1/HW04.Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HW04.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sum();
+        }
+
+        static void Sum()
+        {
+            Console.Write($"Enter the first number: ");
+            double num1 = CheckInput();
+            Console.Write($"Enter the second number: ");
+            double num2 = CheckInput();
+            Console.WriteLine($"Result: {num1} + {num2} = {num1 + num2}");
+        }
+
+        static double CheckInput()
+        {
+            double num;
+            while (!double.TryParse(Console.ReadLine(), out num))
+                Console.WriteLine($"Error. Try again:");
+            return num;
+        }
+    }
+}
---
+++ HW04.Task1/HW04.Task5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HW04.Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char lastLatter = 'Z';
+            char firstLatter = 'A';
+            for (int i = Convert.ToInt32(lastLatter); i > Convert.ToInt32(firstLatter) - 1; i--)
+            {
+                Console.Write($"{Convert.ToChar(i)} ");
+            }
+        }
+    }
+}
