41b41f3d975512ec0605df29d8e6ffcf7c440156
Ruslan Mikhno <r.mikhno92@gmail.com>
2020-06-17 01:21:04 +0300

    the first task pompleted

---
+++ HW06/Task1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write($"Enter the name of the poem: ");
+            string namePoem = TheNameOrAuthorOfThePoem();
+
+            Console.Write($"Enter the author of a poem: ");
+            string author = TheNameOrAuthorOfThePoem();
+
+            string poem = UserPoem();
+            Console.Clear();
+            string[] poemArray = poem.Split(";");
+            Replace(poemArray);
+
+            PrintPoem(poemArray, namePoem, author);
+        }
+
+        static string TheNameOrAuthorOfThePoem()
+        {
+            string str = Console.ReadLine();
+            if (str == string.Empty)
+                return null;
+            else
+                return str;
+        }
+
+        static string UserPoem()
+        {
+            Console.WriteLine($"Enter your verse. Instead of a space, please, use \";\":");
+            string str = Console.ReadLine();
+            return str;
+        }
+
+        static string[] Replace(string[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = arr[i].Replace("O", "A");
+                arr[i] = arr[i].Replace("o", "a");
+            }
+
+            return arr;
+        }
+
+        static void PrintPoem(string[] arr, string namePoem, string author)
+        {
+            Console.WriteLine($"\t{namePoem}");
+            foreach (var i in arr)
+                Console.WriteLine(i);
+
+            Console.WriteLine($"\t\t{author}");
+        }
+    }
+}
